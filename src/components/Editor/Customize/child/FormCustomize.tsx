307183eac8448afc67ac91793758a901e89d8dba
tanjir-mahabub <md.tanjir.mahabub@gmail.com>
2024-02-01 14:45:48 +0600

    Editor: Frag and Form Section desgined and developed

--- src/components/Editor/Customize/child/FormCustomize.tsx
+++ src/components/Editor/Customize/child/FormCustomize.tsx
@@ -1,8 +1,15 @@
+import StickerSelector from "@/components/Homepage/Selector/StickerSelector"
 
 const FormCustomize = () => {
     return (
-        <div>
-            <h1>FormCustomize</h1>
+        <div className="w-full overflow-y-auto">
+            <div className="p-4 space-y-5 overflow-y-auto">
+                <h2 className="text-sm sm:text-lg font-bold">Välj bakgrundsfärg</h2>
+            </div>
+
+            <div className="grid grid-cols-2 justify-start items-center gap-3 h-[10%] p-4">
+                <StickerSelector />
+            </div>
         </div>
     )
 }
